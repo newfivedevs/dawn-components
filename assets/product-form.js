438cefa8f959c2ee504772f8441505c09f82cb0e
roninewfive <109126623+roninewfive@users.noreply.github.com>
2022-08-07 17:26:31 +0800

    complete the look v2 and v3

--- assets/product-form.js
+++ assets/product-form.js
@@ -66,6 +66,7 @@ if (!customElements.get('product-form')) {
           console.error(e);
         })
         .finally(() => {
+
           this.submitButton.classList.remove('loading');
           if (this.cart && this.cart.classList.contains('is-empty')) this.cart.classList.remove('is-empty');
           if (!this.error) this.submitButton.removeAttribute('aria-disabled');
